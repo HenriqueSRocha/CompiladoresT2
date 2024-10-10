318a3b596b3c86ff1a8351c51b55a84a8c294cae
henriquesrocha <henrique.s.rochaa@gmail.com>
2024-10-10 01:23:16 -0300

    first commit

---
+++ tokens.h
@@ -0,0 +1,17 @@
+#define NUM 256
+#define ID 257
+#define INT 258
+#define GE 259
+#define IF 260
+#define ELSE 261
+#define WHILE 262
+#define FOR 267
+#define LE 263
+#define AND 266
+#define OR 268
+#define EQ 269
+#define FLOAT 270
+#define VOID 271
+#define STRING 272
+#define ASPAS 273
+#define RETURN 274
